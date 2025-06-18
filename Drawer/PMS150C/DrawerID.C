ad85740c988cc40d353457443553ce0c5f39107d
= <andywm@users.noreply.github.com>
2025-06-18 18:36:32 +0100

    Add initial PMS150C drawer id code.

---
+++ Drawer/PMS150C/DrawerID.C
@@ -0,0 +1,50 @@
+﻿/*
+#include	"extern.h"
+
+// PA0: Input, PA3: Output
+
+void	FPPA0 (void)
+{
+	.ADJUST_IC SYSCLK=IHRC/2;
+	PA = 0b_0000_0000; // Port A Data register
+	PAC = 0b_1111_1110; // Port A Control register, 0/1: input/output
+	PAPH = 0b_0000_0001; // Port A Pull-High Register 0/1: disable/enable
+	$ PADIER 0b_1111_1001; // Port A Digital Input Enable Register, 1/0: enable/disable, Bit 2:1 is reserved
+	while (1)
+	{
+		if( pa.0 == 0)
+		{
+			pa.3 = 1;
+
+		}
+		else 
+		{
+			pa.3 = 1;
+		}
+//		...
+//		wdreset;
+
+	}
+}*/
+
+#include	"extern.h"
+
+void	FPPA0 (void)
+{
+	.ADJUST_IC	SYSCLK=IHRC/2
+
+	PA = 0b_0000_0000; // Port A Data register
+	PAC = 0b_1111_1111; // Port A Control register, 0:input / 1:output
+	PAPH = 0b_0000_0000; // Port A Pull-High Register, 0:disable / 1:enable
+	$ PADIER 0b_1111_1001; // Port A Digital Input Enable Register, 1:enable / 0:disable, Bit 2:1 is reserved
+
+	pa.0 = 1;
+	pa.3 = 1;
+
+	while (1)
+	{
+		pa.0 = 1;
+		pa.3 = 1;
+	//	wdreset;
+	}
+}
